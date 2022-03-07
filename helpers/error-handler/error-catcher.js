e3a1fd392358097ecc819b9c9f972c82ef11a66d
emmanr <emman.info22@gmail.com>
2022-03-08 01:40:45 +0800

    fixed bug in error catcher - status code not set

--- helpers/error-handler/error-catcher.js
+++ helpers/error-handler/error-catcher.js
@@ -1,7 +1,8 @@
 exports.throwError = (status, msg) => {
   const error = new Error(msg);
-  error.statusCode = 422;
+  error.statusCode = status;
   throw error;
+  // note that if we are inside async await, we should use next(error), but most cases, this will throw an error and try-"catch" will received it so it is okay
 };
 
 exports.errorCatcher = (err, next) => {
