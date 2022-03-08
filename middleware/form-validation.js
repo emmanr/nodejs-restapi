fe571eb8ef880b0cb4f343f7fcafbee9d38998f4
emmanr <emman.info22@gmail.com>
2022-03-08 13:42:20 +0800

    update form validation - added comment

--- middleware/form-validation.js
+++ middleware/form-validation.js
@@ -1,4 +1,5 @@
 const { body } = require('express-validator');
 
 exports.title = body('title', 'Title length should be min of 5 and max of 100.').trim().isLength({min: 5, max: 100});
-exports.content = body('title', 'Content length should be min of 5 and max of 1000.').trim().isLength({min: 5, max: 1000});
+exports.content = body('content', 'Content length should be min of 5 and max of 1000.').trim().isLength({min: 5, max: 1000});
+// exports.name = body('input field name', 'message').validation_methods()
