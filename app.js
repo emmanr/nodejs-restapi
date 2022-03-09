ae3196e94344181d1740d581c62f5c52f1c6427a
emmanr <emman.info22@gmail.com>
2022-03-09 20:42:20 +0800

    clean-up appjs by removing NOTE comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// setting up express and create an instance of it
 const express = require('express'), app = express();
 
 // modules
@@ -25,23 +24,15 @@ const authRoutes = require('./routes/auth');
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
-  { flags: 'a' } // new data will append
+  { flags: 'a' }
 );
 
-app.use(helmet()); // For setting secure response headers
-app.use(compression()); // Compressing assets (css and js) Note: that we may not need this if our host has a built-in compression
+app.use(helmet());
+app.use(compression());
 app.use(morgan('combined', { stream: accessLogStream }));
-// it is best to just create a log file to display all the logs instead in the console
-// to log more details in our consoles (browser type, methods, host ...)
-
-// will not use urlEncoded => this parse data in format of x-www-for-urlencoded, this is the default data if submitted through a <form> post request - app.use(bodyParser.urlEncoded());
-// instead will use bodyParser with json() method
-app.use(bodyParser.json()); // will parse json data from incoming request or in format of application/json
+app.use(bodyParser.json());
 app.use(multerHelper);
-
-// storing images to root/images(__dirname, 'images') from a request of "/images"
-app.use('/images', express.static(path.join(__dirname, 'images'))); // the path in our root folder with /images(__dirname, 'images') will be serve staticly
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cors);
 
 // routes
