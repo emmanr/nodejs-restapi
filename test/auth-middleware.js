bc1ff9fa1d2254a1f85ba27cdfe91132822aac36
emmanr <emman.info22@gmail.com>
2022-03-10 19:55:49 +0800

    added 3 test for is-auth middleware

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,23 +1,41 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', function() {
-  const req = {
-    get: function() {
-      return null;
+describe('Auth Middleware', function() {
+  // describe('Header title', function() {}); you can have a nested describe function
+
+  it('should throw an error if no authorization header is present', function() {
+    const req = {
+      get: function() {
+        return null;
+      }
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated!');
+  });
+
+  it('should throw an error if the authorization header is only one string', function() {
+    const req = {
+      get: function() {
+        return 'xyz';
+      }
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function() {
+    const req = {
+      get: function() {
+        return 'xyz';
+      }
+    };
+    jwt.verify = function() {
+      return { userId: 'abc' }
     }
-  };
-
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated!');
-});
-
-it('should throw an error if the authorization header is only one string', function() {
-  const req = {
-    get: function() {
-      return 'xyz';
-    }
-  };
-
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+  });
 });
